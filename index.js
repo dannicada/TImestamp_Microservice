2a19599588354594791f933f6a8a0676a1a38652
dannicada <dannynwangwu@gmail.com>
2019-10-23 22:56:24 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require("http");
+
+const requestHandler = (req, res) => {
+    if (req.url === "/")
+    console.log(req.url);
+    res.end('Hello world');
+};
+
+const server = http.createServer(requestHandler);
+server.listen(process.env.PORT || 4100, err => {
+    if (err) throw err;
+
+    console.log('Server running on PORT ${server.address().port}');
+});
