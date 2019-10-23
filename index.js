0bdfadb08dacd1747dd17b90b4621cd1bedd27c6
dannicada <dannynwangwu@gmail.com>
2019-10-23 23:27:13 +0100

    added html route

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const http = require("http");
+const fs = require("fs");
 
 const requestHandler = (req, res) => {
-    console.log(req.url);
-    res.end('Hello world');
+    if (req.url === "/"){
+        fs.readFile("views/index.html", "utf8", (err, html) => {
+            if (err) throw err;
+
+            res.writeHead(200, {"content-Type": "text/html"});
+            res.end(html);
+        });
+    }
 };
 
 const server = http.createServer(requestHandler);
